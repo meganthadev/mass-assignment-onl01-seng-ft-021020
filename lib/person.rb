0b605bf2d38c63ecddf5bff16ab014937369bd8c
Megan Horsfield <meganthadev@gmail.com>
2020-03-03 03:33:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,11 @@ class Person
 
   attr_accessor : :name, :birthday, :hair_color, :eye_color, :height, 
   :weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width
+  :wrist_size, :glove_size, :pant_length, :pant_width
+  
+  def intitialize(attributes) 
+    attributes.each { |key, value| self.send(("#{key}="), value)}
+  end   
 
 
 end
